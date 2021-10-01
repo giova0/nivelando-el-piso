0fdea6d719f324d2300615340783cfc8302489e0
giova0 <giova0@gmail.com>
2021-10-01 11:25:32 -0500

    AcumulaLLuvia

--- main.ts
+++ main.ts
@@ -1,43 +1,19 @@
-basic.forever(function () {
-    if (input.acceleration(Dimension.X) >= -5 && input.acceleration(Dimension.X) <= 5) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            `)
-    } else if (input.acceleration(Dimension.X) >= -100 && input.acceleration(Dimension.X) <= -5) {
-        basic.showLeds(`
-            . . # . .
-            . . # # .
-            . # # # #
-            . . # # .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.X) >= -1023 && input.acceleration(Dimension.X) <= -100) {
-        basic.showLeds(`
-            . . # . .
-            . . # # .
-            # # # # #
-            . . # # .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.X) <= 100 && input.acceleration(Dimension.X) > 5) {
-        basic.showLeds(`
-            . . # . .
-            . # # . .
-            # # # # .
-            . # # . .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.X) > 100 && input.acceleration(Dimension.X) <= 1023) {
-        basic.showLeds(`
-            . . # . .
-            . # # . .
-            # # # # #
-            . # # . .
-            . . # . .
-            `)
+let v_dado = 0
+let AguaAcumulada = 0
+let AcumulaValorDos = 0
+let Promedio = 0
+input.onButtonPressed(Button.A, function () {
+    v_dado = 0
+    AguaAcumulada = 0
+    AcumulaValorDos = 0
+    for (let index = 0; index < 7; index++) {
+        if (v_dado == 2) {
+            AcumulaValorDos = AcumulaValorDos + 1
+        }
+        v_dado = randint(0, 6)
+        AguaAcumulada += v_dado
     }
+    Promedio = AguaAcumulada / 7
+    basic.showNumber(Promedio)
+    basic.showNumber(AcumulaValorDos)
 })
